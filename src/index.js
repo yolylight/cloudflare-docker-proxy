4132a1f8a87dd7787bacb335702ce62e95208299
yolylight <yolylight@hotmail.com>
2024-07-02 13:42:45 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.walletong.win": dockerHub,
+  "quay.walletong.win": "https://quay.io",
+  "gcr.walletong.win": "https://gcr.io",
+  "k8s-gcr.walletong.win": "https://k8s.gcr.io",
+  "k8s.walletong.win": "https://registry.k8s.io",
+  "ghcr.walletong.win": "https://ghcr.io",
+  "cloudsmith.walletong.win": "https://docker.cloudsmith.io",
+  "ecr.walletong.win": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
